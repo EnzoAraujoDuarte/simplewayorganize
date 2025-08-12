7f551651b954fa118ba1a8f8131fffbe3745b8e7
Enzo Araujo <140770206+EnzoAraujoDuarte@users.noreply.github.com>
2025-08-12 13:14:25 -0300

    Delete app directory

--- app/test.tsx
+++
@@ -1,8 +0,0 @@
-export default function TestPage() {
-  return (
-    <div>
-      <h1>Teste - SimpleWay Atacado S.A.</h1>
-      <p>Se você está vendo esta página, o React está funcionando!</p>
-    </div>
-  );
-} 
