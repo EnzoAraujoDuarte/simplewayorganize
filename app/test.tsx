233f0dec0f3e7ee49af4a4d4b2cb3be71c613217
Enzo Araujo <140770206+EnzoAraujoDuarte@users.noreply.github.com>
2025-08-12 13:14:53 -0300

    Add files via upload

---
+++ app/test.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div>
+      <h1>Teste - SimpleWay Atacado S.A.</h1>
+      <p>Se você está vendo esta página, o React está funcionando!</p>
+    </div>
+  );
+} 
